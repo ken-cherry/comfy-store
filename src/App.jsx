1f893ef8969127bf902cd4dc4762381e3cbcf0c8
ken-cherry <ken@cherrywebdev.com>
2024-01-17 22:56:47 -0600

    Created: createBrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import {
   About,
   Cart,
   Checkout,
   Error,
+  HomeLayout,
   Landing,
   Login,
   Orders,
@@ -12,10 +14,62 @@ import {
   SingleProduct,
 } from "./pages";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: "products",
+        element: <Products />,
+      },
+      {
+        path: "products/:id",
+        element: <SingleProduct />,
+      },
+      {
+        path: "cart",
+        element: <Cart />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "checkout",
+        element: <Checkout />,
+      },
+      {
+        path: "products",
+        element: <Products />,
+      },
+      {
+        path: "orders",
+        element: <Orders />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+    errorElement: <Error />,
+  },
+]);
+
 function App() {
   return (
     <>
-      <h1 className="text-7xl font-bold underline">Tailwind project</h1>;
+      <RouterProvider router={router} />
     </>
   );
 }

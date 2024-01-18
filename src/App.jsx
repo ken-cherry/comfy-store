bde97ef3cc2a4698f36e08bc128abf3051115bcc
ken-cherry <ken@cherrywebdev.com>
2024-01-17 22:10:16 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-7xl font-bold underline">Tailwind project</h1>;
+    </>
+  );
+}
+
+export default App;

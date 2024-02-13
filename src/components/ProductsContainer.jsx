3dac92de65407eac9bddf7f0564a7098fbdc629b
ken-cherry <ken@cherrywebdev.com>
2024-02-13 10:31:15 -0600

    Created: Filters, PaginationContainer and ProductsContainer components.

---
+++ src/components/ProductsContainer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ProductsGrid from "./ProductsGrid";
+import ProductsList from "./ProductsList";
+const ProductsContainer = () => {
+  return (
+    <>
+      <ProductsList />
+      <ProductsGrid />
+    </>
+  );
+};
+
+export default ProductsContainer;
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
+import PaginationContainer from "./PaginationContainer";
+
 export { default as FormInput } from "./FormInput";
 export { default as SubmitBtn } from "./SubmitBtn";
 export { default as Header } from "./Header";
@@ -6,3 +8,6 @@ export { default as Hero } from "./Hero";
 export { default as ErrorElement } from "./ErrorElement";
 export { default as FeaturedProducts } from "./FeaturedProducts";
 export { default as SectionTitle } from "./SectionTitle";
+export { default as Filters } from "./Filters";
+export { default as ProductsContainer } from "./ProductsContainer";
+export { default as PaginationContainer } from "./PaginationContainer";
